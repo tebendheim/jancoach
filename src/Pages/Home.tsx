65e51ba984db040cedde011e83318ea4da074add
Tom-Elbin Bendheim <tom-elbinbendheim@Tom-Elbins-MacBook-Pro.local>
2023-02-16 20:23:08 +0100

    legger til bilde og ny layout

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -3,13 +3,13 @@ import {Navigate} from 'react-router-dom'
 
 function Home() {
 	return (
-		<section className='h-auto bg'>
-				<div className="absolute to-transparent
-					bg-[url('../../Bilder/janPaaFjellet.png')]
-					bg-no-repeat
-					bg-cover bg-clip-content min-w-full min-h-full z-0 top-0 left-0">
-					<a  className="rounded-full w-40 h-10 absolute bottom-1/3 left-32  text-center align-bottom backdrop-blur-xl backdrop-brightness-50 text-white" href="/kontakt">Kontakt meg</a>
-		
+		<section className='h-auto h-max-full min-h-fit bg'>
+			<div className="absolute to-transparent
+				bg-[url('../../Bilder/janPaaFjellet.png')]
+				bg-no-repeat
+				bg-cover bg-clip-content min-w-full min-h-full z-0 top-0 left-0">
+				<p className="text-lg absolute bottom-0 left-32 text-center ">Det handler om hel ved</p>
+				<a  className="rounded-full w-40 h-10 absolute bottom-1/3 left-32  text-center align-bottom backdrop-blur-xl backdrop-brightness-50 text-white" href="/kontakt">Kontakt meg</a>
 			</div>
 			<div className='text-black dark:text-white h-auto'>
 				<i className='fas fa-bars text-green-800 text-'></i> Home
