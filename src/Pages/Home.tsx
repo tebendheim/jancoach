56fca1fbfa1c118b4e00163b50875993a04c1299
Tom-Elbin Bendheim <tom-elbinbendheim@Tom-Elbins-MacBook-Pro.local>
2023-02-21 15:43:36 +0100

    nytt forsøk på å ikke bruke img tag for forsidebilde

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -4,17 +4,13 @@ import fBilde from "../../Bilder/janPaaFjellet.png";
 
 function Home() {
 	return (
-		<section className='h-auto h-max-full min-h-fit bg'>
-			<img src={fBilde} className=" absolute min-h-full min-w-full overflow-clip z-0 top-0 left-0">
-			</img>
-			
-			<div className='text-black dark:text-white h-auto'>
-				<i className='fas fa-bars text-green-800 text-'></i> Home
-			</div>
-			<div className="bg-green-600 h-screen w-32"> test</div>
-			<div className='bg-nav h-screen w-55'></div>
-			<div className='bg-red-600 h-screen w-55'></div>
-		</section>
+		<div className="absolute to-transparent
+		bg-[url('/Bilder/janPaaFjellet.png')]
+		bg-no-repeat
+		bg-cover bg-clip-content min-w-full min-h-full z-0 top-0 left-0">
+		<p className="text-lg absolute bottom-0 left-32 text-center ">Det handler om hel ved</p>
+		<a  className="rounded-full w-40 h-10 absolute bottom-1/3 left-32  text-center align-bottom backdrop-blur-xl backdrop-brightness-50 text-white" href="/kontakt">Kontakt meg</a>
+	</div>
 
 	);
 }
@@ -28,3 +24,18 @@ export default Home;
 				<p className="text-lg absolute bottom-0 left-32 text-center ">Det handler om hel ved</p>
 				<a  className="rounded-full w-40 h-10 absolute bottom-1/3 left-32  text-center align-bottom backdrop-blur-xl backdrop-brightness-50 text-white" href="/kontakt">Kontakt meg</a>
 			</div> */}
+
+
+
+
+		// 	<section className='h-auto h-max-full min-h-fit bg'>
+		// 	<img src={fBilde} className=" absolute min-h-full min-w-full overflow-clip z-0 top-0 left-0">
+		// 	</img>
+			
+		// 	<div className='text-black dark:text-white h-auto'>
+		// 		<i className='fas fa-bars text-green-800 text-'></i> Home
+		// 	</div>
+		// 	<div className="bg-green-600 h-screen w-32"> test</div>
+		// 	<div className='bg-nav h-screen w-55'></div>
+		// 	<div className='bg-red-600 h-screen w-55'></div>
+		// </section>
