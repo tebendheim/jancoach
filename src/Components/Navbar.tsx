3ebc3d21986495095b07c1b8bc97329c85833424
Tom-Elbin Bendheim <tom-elbinbendheim@Tom-Elbins-MacBook-Pro.local>
2023-04-02 21:22:01 +0200

    Har endret spacing på ul i navbar med mer. Den fungerer fint nå

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -9,16 +9,25 @@ const Navbar: React.FC = () => {
 	const handleClick = () => {
 		onToggleDD(!toggleDD);
 	};
+	
+	React.useEffect(() => {
+		const handleResize = () => {
+			onToggleDD(false);
+		};
+		window.addEventListener('resize', handleResize);
+	})
+	
+	
 	const { isDarkMode, toggle } = useDarkMode();
 /*Alternativ på nav: bg-transparent backdrop-blur-xl dark:backdrop-brightness-50 backdrop-brightness-125 */
 	return (
-		<section className="bg-white dark:bg-black row-start-1 row-span-2 col-span-full h-fit">
+		<section className="bg-white dark:bg-black row-start-1 col-span-full row-span-2">
 			{/*  "backdrop-filter backdrop-blur-2xl  h-16 md:h-24 dark:bg-black bg-opacity-75 sticky w-screen top-0  border-gray-200 px-2 md:px-4 z-30  md:flex md:flex-row" */}
-			<nav className={``}>
+			<nav className={`fixed backdrop-filter backdrop-blur-2xl w-full dark:backdrop-brightness-50 backdrop-brightness-125 `}>
 				{/* grid ${toggleDD ? "grid-rows-3" : "grid-rows-1" } md:grid-rows-1 grid-cols-12 md:container md:flex md:flex-wrap md:justify-between place-items-center */}
 				<div className={`grid ${toggleDD ? "grid-rows-navBar2":"grid-rows-navBar1"} md:grid-rows-1 md:h-24 auto-rows-auto grid-cols-12 items-center`}>
 					{/* col-start-2 w-fit text-xl whitespace-nowrap dark:text-white font-bold dark:hover:text-gray-300 */}
-					<a href="/" className='text-white whitespace-nowrap col-start-2 justify-self-start row-start-1'>
+					<a href="/" className=' whitespace-nowrap col-start-2 justify-self-start row-start-1 text-slate-900 dark:text-gray-50 dark:hover:text-gray-400'>
 						HEL VED
 					</a>
 					{/* 'col-end-13 w-fit h-fit text-sm text-gray-700 rounded-lg md:hidden hover:bg-gray-100  hover:ring-2 dark:text-white dark:hover:bg-gray-700 */}
@@ -51,14 +60,14 @@ const Navbar: React.FC = () => {
 
 						{/*her må jeg få fjernet Hidden ved å trykke på hamburger*/}
 						{/* <ul id='navbar-default' className={`${toggleDD ? "flex": "hidden"} md:flex flex-col rounded-lg border md:flex-row md:mt-0 md:text-sm md:font-medium md:border-0  dark:to-slate-700 row-start-2`}>   {/* {bg-transparent backdrop-blur-xl dark:backdrop-brightness-50 backdrop-brightness-125} */}
-						<ul className={` ${toggleDD ? "flex-col" : "hidden"} md:flex col-start-2 justify-self-start row-start-2 md:row-start-1  md:justify-self-end md:col-end-12`}>
+						<ul className={` ${toggleDD ? "flex-col" : "hidden"} justify-center place-items-center md:flex col-start-6 justify-self-start row-start-2  md:row-start-1  md:col-start-7 md:col-span-5 lg:col-start-8 lg:col-span-4 md:justify-self-stretch md:justify-between`}>
 						{/*  <li>
 							<button  className=" justify-self-start rounded md:bg-transparent   text-slate-900 text-lg dark:text-gray-50 dark:hover:text-gray-400 text-blue-500">
 							
 						</button> 
 							</li> */}
-							<li>
-								<button onClick={toggle}>
+							<li className="my-4 text-center">
+								<button className="align-middle" onClick={toggle}>
 									{isDarkMode ? (
 									<SunIcon className="h-6 w-6 text-blue-600" />
 									) : (
@@ -66,7 +75,7 @@ const Navbar: React.FC = () => {
 									)}
 								</button>
 							</li>
-							<li>
+							<li className="my-4 text-center">
 								<Link
 									to='/kontakt'
 									className=' justify-self-start rounded md:bg-transparent   text-slate-900 text-lg dark:text-gray-50 dark:hover:text-gray-400 '
@@ -75,7 +84,7 @@ const Navbar: React.FC = () => {
 									Kontakt
 								</Link>
 							</li>
-							<li>
+							<li className="my-4 text-center">
 								<Link
 									to='/tjenester'
 									className='rounded md:bg-transparent   text-slate-900 text-lg dark:text-gray-50 dark:hover:text-gray-400 '
@@ -83,7 +92,7 @@ const Navbar: React.FC = () => {
 									Tjenester
 								</Link>
 							</li>
-							<li>
+							<li className="my-4 text-center">
 								<Link
 									to='om'
 									className='whitespace-nowrap rounded md:bg-transparent  text-slate-900 text-lg dark:text-gray-50 dark:hover:text-gray-400 '
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -7,13 +7,13 @@ import Navbar from '../Components/Navbar'
 function Home():JSX.Element {
 	// h-full w-full relative -top-24 left-0
 	return (
-			<section className="left-0 col-span-full  row-start-2 ">
+			<section className="col-span-full row-start-1">
 				<section>
-					<div className="h-screen to-transparent
+					<div className="h-screen w-full to-transparent
 						bg-[url('../../Bilder/janPaaFjellet.png')]
 						bg-no-repeat
-						bg-cover bg-clip-content min-w-full min-h-full z-0 left-0">	
-						<div className="flex justify-items-start w-fit relative top-2/3 left-36 ">
+						bg-cover bg-clip-content z-0">	
+						<div className="flex ">
 							<div className="rounded-full w-fit h-fit p-4 font-bold  mr-2 bg-yellow-300 bg-opacity-80"><Link  className="text-center align-bottom  text-black " to="/kontakt">Kontakt meg</Link></div>
 							<div className="rounded-full w-fit h-fit p-4 font-bold bg-white dark:bg-black bg-opacity-50"><Link  className="text-center align-bottom text-black dark:text-white " to="/om">Om</Link></div>
 						</div>
