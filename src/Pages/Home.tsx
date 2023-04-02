9e0bffa63e830c5b34c3302e95f8c9bacd0034d9
Tom-Elbin Bendheim <tom-elbinbendheim@Tom-Elbins-MacBook-Pro.local>
2023-04-02 22:18:25 +0200

    Forrige versjon fungerte for å bruke 'janPaaFjellet'. Tester nå om denne versjonen fungerer.

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {Navigate, Link} from 'react-router-dom'
 import OmHome from '../Components/OmHome'
 import Navbar from '../Components/Navbar'
-import JanPaaFjeler from '../../Bilder/janPaaFjellet.png'
+import JanPaaFjellet from '../../Bilder/janPaaFjellet.png'
 
 
 function Home():JSX.Element {
@@ -11,9 +11,10 @@ function Home():JSX.Element {
 			<section className="col-span-full row-start-1">
 				<section>
 					<div className="h-screen
-						bg-[url('/Bilder/janPaaFjellet.png')]
 						bg-no-repeat
-						bg-cover bg-clip-content z-0">	
+						bg-cover bg-clip-content z-0"  style={{
+							backgroundImage: `url(${JanPaaFjellet})`,
+						  }}>	
 						<div className="flex ">
 							<div className="rounded-full w-fit h-fit p-4 font-bold  mr-2 bg-yellow-300 bg-opacity-80"><Link  className="text-center align-bottom  text-black " to="/kontakt">Kontakt meg</Link></div>
 							<div className="rounded-full w-fit h-fit p-4 font-bold bg-white dark:bg-black bg-opacity-50"><Link  className="text-center align-bottom text-black dark:text-white " to="/om">Om</Link></div>
