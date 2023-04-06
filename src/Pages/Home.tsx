4bfcad8bff07372594ab55c198818a0fc3b599a3
Tom-Elbin Bendheim <tom-elbinbendheim@Tom-Elbins-MacBook-Pro.local>
2023-04-06 18:36:09 +0200

    Endret main page slik at annenhver start blir riktig

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -39,9 +39,9 @@ function Home():JSX.Element {
 						<h2 className="pt-4 font-semibold dark:text-white">What is Lorem Ipsum?</h2>
 						<p className="pt-2 mb-6 dark:text-white">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
 					
-						<MyButton className="w-fit py-2 self-end" buttonText = "Gratis veiledningstime" to="/kontakt" />
+						<MyButton className="w-fit py-2 self-start" buttonText = "Gratis veiledningstime" to="/kontakt" />
 					</div>
-					<div className=" row-start-2 col-start-2 w-full col-span-10 h-96 md:col-start-7 md:row-start-1  bg-slate-400 row-span-1 my-12 md:col-span-5"></div>
+					<div className=" row-start-2 col-start-2 w-full col-span-10 h-96 md:col-start-2 md:row-start-1  bg-slate-400 row-span-1 my-12 md:col-span-5"></div>
 				</section>
 				<section className=" row-start-5 row-span-1 col-start-1 col-span-12 grid grid-rows-4-auto grid-cols-12 py-12 ">
 				<div className="col-start-2 md:col-start-2 md:col-span-4 col-span-10 row-start-1 row-span-1 flex flex-col max-h-full min-h-fit">
