cf3aa6599f24f39a5d06426ceb98e1d3d816c2f0
Tom-Elbin Bendheim <tom-elbinbendheim@Tom-Elbins-MBP.home>
2024-06-12 00:07:43 +0200

    lagt til navbar i om på nytt

--- src/Pages/Om.tsx
+++ src/Pages/Om.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Navbar from "../Components/Navbar";
 
 const Om = () => {
   return (
     <section className="relative ">
+      <Navbar />
       <div>Om</div>
     </section>
   );
